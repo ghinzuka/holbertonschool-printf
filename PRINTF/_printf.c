377ac5d1bd72b3c8bbef8e029d683c858feadcea
ghinzuka <baptiste.pouquerou@gmail.com>
2024-03-25 13:39:53 +0000

    creating the main function to try the putchar function

--- PRINTF/_printf.c
+++ PRINTF/_printf.c
@@ -6,6 +6,35 @@
  */
 int _printf(const char *format, ...)
 {
+int i, j;
+va_list par;
 
+type s[] {
+	{'c', printchar},
+	{'s', printstr},
+	{'%', printpercent},
+	{'d', printdec},
+	{'i', printint}
+};
+
+va_start (par, format);
+
+i = 0;
+
+	while (format && format[i])
+	{
+		j = 0; 
+		while (s[j].t)
+		{
+			if (s[j].t == format[i])
+			{
+				s[j].f(par);
+			}
+			j++;
+		}
+		i++;
+	}
+va_end(par);
+_putchar(\n);
 }
 
