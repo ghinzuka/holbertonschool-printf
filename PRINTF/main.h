dc1ea1a7ba3a4d1cfc5fc4b1deddf5dac71e4b2b
ghinzuka <baptiste.pouquerou@gmail.com>
2024-03-25 12:34:02 +0000

    header file

---
+++ PRINTF/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+int _printf(const char *format, ...);
+
+/**
+ *
+ *
+ *
+ */
+
+typedef struct type
+{
+	char t;
+	void(*f)()
+}
+
+#endif
